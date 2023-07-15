0b941b5dd1de92d0cfe641b001ddcae7f7bb1127
Can202 <mgoopazo@gmail.com>
2023-07-14 23:49:05 -0400

    change '' for ""
    for some reason windows, does not work with curl if you use ''

--- curlcommand.py
+++ curlcommand.py
@@ -6,6 +6,6 @@ name = input ("Name the file: ")
 
 save_it = input ("Where do you want to save it? ")
 
-command = "curl -X POST -H 'Name: " + name + "' -F 'file=@" + file_dir +   "' '192.168.1.18:8000/upload/" + save_it + "'"
+command = "curl -X POST -H \"Name: " + name + "\" -F \"file=@" + file_dir +   "\" \"192.168.1.18:8000/upload/" + save_it + "\""
 os.system(command)
 
